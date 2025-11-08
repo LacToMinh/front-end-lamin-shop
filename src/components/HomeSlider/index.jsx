c373de1d3a37b7c166d7a6b27f2c2a326feb8e44
Lac To Minh <lacminh161@gmail.com>
2025-11-08 19:55:20 +0700

    feat: complete responsive homeslider

--- src/components/HomeSlider/index.jsx
+++ src/components/HomeSlider/index.jsx
@@ -2,27 +2,41 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Autoplay, Navigation } from "swiper/modules";
-const HomeSlider = (props) => {
 
+const HomeSlider = ({ data = [] }) => {
   return (
-    <Swiper
-      navigation={true}
-      modules={[Navigation, Autoplay]}
-      autoplay={{
-        delay: 3000,
-        disableOnInteraction: false,
-      }}
-      className="mySwiper h-[600px]"
-    >
-      {props?.data?.length !== 0 &&
-        props?.data?.map((item, index) => {
-          return (
+    <div className="homeSlider w-full">
+      <Swiper
+        navigation={true}
+        modules={[Navigation, Autoplay]}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true, // ✅ dừng khi hover
+        }}
+        loop={true}
+        centeredSlides={true}
+        grabCursor={true}
+        speed={800}
+        className="mySwiper w-full"
+      >
+        {data.length > 0 &&
+          data.map((item, index) => (
             <SwiperSlide key={index}>
-              <img src={item?.images[0]} alt="" className="w-full" />
+              <div className="relative w-full">
+                <img
+                  src={item?.images?.[0]}
+                  alt={`slide-${index}`}
+                  className="w-full h-[250px] sm:h-[350px] md:h-[500px] lg:h-[600px] xl:h-[700px] object-fill md:object-cover object-center transition-transform duration-500 hover:scale-[1.02]"
+                />
+
+                {/* ✅ Optional: overlay nhẹ để tăng độ tương phản chữ (nếu bạn muốn thêm text sau này) */}
+                <div className="absolute inset-0 bg-black/10"></div>
+              </div>
             </SwiperSlide>
-          );
-        })}
-    </Swiper>
+          ))}
+      </Swiper>
+    </div>
   );
 };
 
